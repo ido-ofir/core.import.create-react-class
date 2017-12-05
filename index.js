8d2135d81137ded5936cef0f6adb82daefa0ea07
ido <ido@dooble.co.il>
2017-12-05 16:28:35 +0200

    changed name to "core.import.create-react-class"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var createReactClass = require('create-react-class');
 
 module.exports = {
-    name: 'core.imports.react-create-class',
+    name: 'core.import.create-react-class',
     imports: {
         'create-react-class': createReactClass,
         createReactClass: createReactClass
